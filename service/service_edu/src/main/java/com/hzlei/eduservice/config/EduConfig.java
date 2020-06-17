f474eae1fc33f2f7870fb28c5100262b70dbc677
hzlei <hzlei@foxmail.com>
2020-06-17 22:40:55 +0800

    添加分页插件

--- service/service_edu/src/main/java/com/hzlei/eduservice/config/EduConfig.java
+++ service/service_edu/src/main/java/com/hzlei/eduservice/config/EduConfig.java
@@ -2,6 +2,7 @@ package com.hzlei.eduservice.config;
 
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,5 +23,14 @@ public class EduConfig {
     public ISqlInjector sqlInjector() {
         return new LogicSqlInjector();
     }
+
+    /**
+     * 分页插件
+     */
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+
 
 }
