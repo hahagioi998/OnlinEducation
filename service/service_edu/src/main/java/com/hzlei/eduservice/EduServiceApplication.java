3ed581ec437124a915b6313d6cb6d88ded0a610e
hzlei <hzlei@foxmail.com>
2020-06-17 21:59:46 +0800

    整合swagger

--- service/service_edu/src/main/java/com/hzlei/eduservice/EduServiceApplication.java
+++ service/service_edu/src/main/java/com/hzlei/eduservice/EduServiceApplication.java
@@ -2,6 +2,7 @@ package com.hzlei.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Description: 启动类
@@ -9,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2020/6/17 20:24
  */
 @SpringBootApplication
+// 主要是为了扫描到公共模块(common)下的 swagger
+@ComponentScan(basePackages = {"com.hzlei"})
 public class EduServiceApplication {
 
     public static void main(String[] args) {
