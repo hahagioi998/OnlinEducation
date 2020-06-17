ea4d58885441d352abc451a1103a5c7000102879
hzlei <hzlei@foxmail.com>
2020-06-17 23:13:04 +0800

    讲师分页查询

--- service/service_edu/src/main/java/com/hzlei/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/hzlei/eduservice/controller/EduTeacherController.java
@@ -1,6 +1,6 @@
 package com.hzlei.eduservice.controller;
 
-
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hzlei.commonutils.R;
 import com.hzlei.eduservice.entity.EduTeacher;
 import com.hzlei.eduservice.service.EduTeacherService;
@@ -38,6 +38,22 @@ public class EduTeacherController {
         boolean flag = eduTeacherService.removeById(id);
         if (flag) return R.ok();
         else return R.error();
+    }
+
+    // 分页查询
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current, @PathVariable long limit) {
+        // 创建page对象
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+        // 调用方法实现分页
+        // 调用方法的时候，底层进行了封装，把分页所有的数据封装到page对象里面了
+        eduTeacherService.page(pageTeacher, null);
+        // 总记录数
+        long total = pageTeacher.getTotal();
+        // 数据list集合
+        List<EduTeacher> records = pageTeacher.getRecords();
+
+        return R.ok().data("total", total).data("rows", records);
     }
 
 
