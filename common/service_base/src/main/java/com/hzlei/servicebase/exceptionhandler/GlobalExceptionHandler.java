de84097c6f3fcf50651a022fa07f835f556de742
hzlei <hzlei@foxmail.com>
2020-06-22 22:32:53 +0800

    logback日志处理

--- common/service_base/src/main/java/com/hzlei/servicebase/exceptionhandler/GlobalExceptionHandler.java
+++ common/service_base/src/main/java/com/hzlei/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.hzlei.servicebase.exceptionhandler;
 
 import com.hzlei.commonutils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @Author hzlei
  * @Date 2020/6/22 21:20
  */
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -17,6 +19,7 @@ public class GlobalExceptionHandler {
     // 指定出现什么异常执行此方法
     @ExceptionHandler(Exception.class)
     public R error(Exception e) {
+        log.error(String.valueOf(e));
         e.printStackTrace();
         return R.error().message("全局异常处理: " + e);
     }
@@ -25,6 +28,7 @@ public class GlobalExceptionHandler {
     // 指定出现什么异常执行此方法
     @ExceptionHandler(ArithmeticException.class)
     public R error(ArithmeticException ae) {
+        log.error(String.valueOf(ae));
         ae.printStackTrace();
         return R.error().message("特定异常处理: " + ae);
     }
@@ -33,6 +37,7 @@ public class GlobalExceptionHandler {
     // 指定出现什么异常执行此方法
     @ExceptionHandler(HzleiException.class)
     public R error(HzleiException he) {
+        log.error(String.valueOf(he));
         he.printStackTrace();
         return R.error().code(he.getCode()).message(he.getMsg());
     }
