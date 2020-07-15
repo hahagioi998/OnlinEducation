aacc14dd9ca343da8a1617971f06c688aebaf1cb
hzlei <hzlei@foxmail.com>
2020-07-15 22:19:23 +0800

    课时操作

--- service/service_edu/src/main/java/com/hzlei/eduservice/controller/EduVideoController.java
+++ service/service_edu/src/main/java/com/hzlei/eduservice/controller/EduVideoController.java
@@ -1,10 +1,12 @@
 package com.hzlei.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.hzlei.commonutils.R;
+import com.hzlei.eduservice.entity.EduVideo;
+import com.hzlei.eduservice.service.EduVideoService;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -19,6 +21,54 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/eduservice/video")
 public class EduVideoController {
+
+    @Resource
+    private EduVideoService videoService;
+
+    /**
+     * 添加小节
+     * @param video
+     * @return
+     */
+    @PostMapping("addVideo")
+    public R addVideo(@RequestBody EduVideo video) {
+        videoService.save(video);
+        return R.ok();
+    }
+
+    /**
+     * 删除小节
+     * @param id
+     * @return
+     * TODO 后面需要完善, 删除小节的时候, 小节里面的视频也要删除
+     */
+    @DeleteMapping("deleteVideo/{id}")
+    public R deleteVideo(@PathVariable String id) {
+        videoService.removeById(id);
+        return R.ok();
+    }
+
+    /**
+     * 修改小节
+     * @param video
+     * @return
+     */
+    @PostMapping("updateVideo")
+    public R updateVideo(@RequestBody EduVideo video) {
+        videoService.updateById(video);
+        return R.ok();
+    }
+
+    /**
+     * 根据 id 查询小节
+     * @param id
+     * @return
+     */
+    @GetMapping("getVideoInfo/{id}")
+    public R getVideoInfo(@PathVariable String id) {
+        EduVideo video = videoService.getById(id);
+        return R.ok().data("video", video);
+    }
 
 }
 
