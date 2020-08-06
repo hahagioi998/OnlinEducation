6f788aaff5802b3e2e092891b76453723b07c86b
hzlei <hzlei@foxmail.com>
2020-08-06 23:02:04 +0800

    注册到 nacos

--- service/service_cms/src/main/java/com/hzlei/educms/CmsApplication.java
+++ service/service_cms/src/main/java/com/hzlei/educms/CmsApplication.java
@@ -3,6 +3,7 @@ package com.hzlei.educms;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -14,6 +15,8 @@ import org.springframework.context.annotation.ComponentScan;
 // 主要是为了扫描到公共模块(common)下的 swagger
 @ComponentScan(basePackages = {"com.hzlei"})
 @MapperScan("com.hzlei.educms.mapper")
+// nacos 注册
+@EnableDiscoveryClient
 public class CmsApplication {
 
     public static void main(String[] args) {
