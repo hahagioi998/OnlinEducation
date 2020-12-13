3624e60b3226a06bba3a853be057557825b5fc35
hzlei <hzlei@foxmail.com>
2020-12-13 21:35:46 +0800

    讲师详情

--- service/service_edu/src/main/java/com/hzlei/eduservice/controller/front/TeacherFrontController.java
+++ service/service_edu/src/main/java/com/hzlei/eduservice/controller/front/TeacherFrontController.java
@@ -1,12 +1,16 @@
 package com.hzlei.eduservice.controller.front;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hzlei.commonutils.R;
+import com.hzlei.eduservice.entity.EduCourse;
 import com.hzlei.eduservice.entity.EduTeacher;
+import com.hzlei.eduservice.service.EduCourseService;
 import com.hzlei.eduservice.service.EduTeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,6 +25,8 @@ public class TeacherFrontController {
 
     @Autowired
     private EduTeacherService teacherService;
+    @Autowired
+    private EduCourseService courseService;
 
     /**
      * 分页查询讲师
@@ -36,6 +42,25 @@ public class TeacherFrontController {
 
         // 返回分页所有数据
         return R.ok().data(map);
+    }
+
+    /**
+     * 讲师详情
+     *
+     * @param teacherId 讲师 id
+     * @return
+     */
+    @GetMapping("getTeacherFrontInfo/{teacherId}")
+    public R getTeacherFrontInfo(@PathVariable String teacherId) {
+        // 根据讲师 id 查询讲师基本信息
+        EduTeacher teacher = teacherService.getById(teacherId);
+
+        // 根据讲师 id 查询所将课程
+        QueryWrapper<EduCourse> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("teacher_id", teacherId);
+        List<EduCourse> courseList = courseService.list(queryWrapper);
+
+        return R.ok().data("teacher", teacher).data("courseList", courseList);
     }
 
 }
